64fa180d506aa2e21e7a86fdc949788f784e5403
ISMAEL SCICLUNA <ismael.scicluna.e25566@mcast.edu.mt>
2020-10-26 09:28:22 +0100

    26/10/2020 creating the basis for our clean architecture

--- SWD62AEP/Presentation/Controllers/SupportController.cs
+++ SWD62AEP/Presentation/Controllers/SupportController.cs
@@ -8,8 +8,26 @@ namespace Presentation.Controllers
 {
     public class SupportController : Controller
     {
+        //this method will be called using a get and its prupose is to load the page
+        [HttpGet]
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(string email,string query)
+        {
+            //ViewData,ViewBag
+            if (string.IsNullOrEmpty(query))
+            {
+                ViewData["feedback"] = "Query was left empty";
+            }
+            else
+            {
+                ViewData["feedback"] = "Thank you for your query, we will get back to you soon";
+            }
+            // ViewBag.feedback = "";
             return View();
         }
     }
